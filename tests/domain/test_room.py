1cab5244900f15f5365248888cb6b26e24b4ee5e
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-05-29 09:49:51 +0000

    ed2-ch03-s05

--- tests/domain/test_room.py
+++ tests/domain/test_room.py
@@ -1,6 +1,8 @@
+import json
 import uuid
-from rentomatic.domain.room import Room
 
+from rentomatic.domain.room import Room
+from rentomatic.serializers.room import RoomJsonEncoder
 
 def test_room_model_init():
     code = uuid.uuid4()
@@ -65,3 +67,29 @@ def test_room_model_comparison():
     room2 = Room.from_dict(init_dict)
 
     assert room1 == room2
+
+
+def test_serialize_domain_room():
+    code = uuid.uuid4()
+
+    room = Room(
+        code=code,
+        size=200,
+        price=10,
+        longitude=-0.09998975,
+        latitude=51.75436293,
+    )
+
+    expected_json = f"""
+        {{
+            "code": "{code}",
+            "size": 200,
+            "price": 10,
+            "longitude": -0.09998975,
+            "latitude": 51.75436293
+        }}
+    """
+
+    json_room = json.dumps(room, cls=RoomJsonEncoder)
+
+    assert json.loads(json_room) == json.loads(expected_json)
